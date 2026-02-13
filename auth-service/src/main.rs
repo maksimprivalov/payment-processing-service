33dafc279ba9ec72cd6d13369b9642fb6a4c3279
maksimprivalov <mprivalovs@gmail.com>
2026-02-13 15:06:40 +0100

    add: gitignore and setup auth-service

---
+++ auth-service/src/main.rs
@@ -0,0 +1,24 @@
+mod config;
+
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+async fn healthcheck() -> &'static str {
+    "OK"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/health", get(healthcheck));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    println!("Running on {}", addr);
+
+    axum::serve(listener, app)
+        .await
+        .unwrap();
+}
