e75ee8e900fa420c16004372e6096e7b646e7ea5
maksimprivalov <mprivalovs@gmail.com>
2026-02-16 14:40:04 +0100

    add : config

---
+++ saga-orchestrator/src/config.rs
@@ -0,0 +1,19 @@
+use std::env;
+
+pub struct Config {
+    pub payment_url: String,
+    pub account_url: String,
+    pub ledger_url: String,
+    pub audit_url: String,
+}
+
+impl Config {
+    pub fn from_env() -> Self {
+        Self {
+            payment_url: env::var("PAYMENT_URL").unwrap(),
+            account_url: env::var("ACCOUNT_URL").unwrap(),
+            ledger_url: env::var("LEDGER_URL").unwrap(),
+            audit_url: env::var("AUDIT_URL").unwrap(),
+        }
+    }
+}
