ba5240aa150140d8a8edf5e89a9348462f98effb
maksimprivalov <mprivalovs@gmail.com>
2026-02-16 14:18:50 +0100

    fix: erased jwt from config

--- audit-service/src/config.rs
+++ audit-service/src/config.rs
@@ -1,8 +1,7 @@
 use std::env;
 
 pub struct Config {
-    pub database_url: String,
-    pub jwt_secret: String,
+    pub database_url: String
 }
 
 impl Config {
@@ -10,8 +9,6 @@ impl Config {
         Self {
             database_url: env::var("DATABASE_URL")
                 .expect("DATABASE_URL not set"),
-            jwt_secret: env::var("JWT_SECRET")
-                .expect("JWT_SECRET not set"),
         }
     }
 }
