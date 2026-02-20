975fe4f1a5c5f40991ef82183fc1a4e25b6d5282
maksimprivalov <mprivalovs@gmail.com>
2026-02-21 00:04:05 +0100

    add: styles & logic for login

--- frontend/src/app/component/auth/login/login.ts
+++ frontend/src/app/component/auth/login/login.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../../../service/api.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrls: ['./login.css']
 })
 export class Login {
 
+  email = '';
+  password = '';
+  loading = false;
+  error = '';
+
+  constructor(private api: ApiService) {}
+
+  login() {
+    this.error = '';
+    this.loading = true;
+
+    this.api.login({
+      email: this.email,
+      password: this.password
+    }).subscribe({
+      next: (res: any) => {
+        localStorage.setItem('token', res.token);
+        this.loading = false;
+        alert('Login successful');
+      },
+      error: () => {
+        this.loading = false;
+        this.error = 'Invalid credentials';
+      }
+    });
+  }
 }
