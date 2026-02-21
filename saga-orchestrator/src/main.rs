55a65d54e0876933fd5ef4214f86fab1e68c0439
maksimprivalov <mprivalovs@gmail.com>
2026-02-21 15:43:44 +0100

    update: saga

--- saga-orchestrator/src/main.rs
+++ saga-orchestrator/src/main.rs
@@ -3,13 +3,21 @@ mod handlers;
 mod error;
 mod models;
 
-use axum::{Router, routing::post};
+use axum::{
+    Router,
+    routing::{post, get},
+};
 use dotenvy::dotenv;
 use tokio::net::TcpListener;
 use tower_http::cors::CorsLayer;
 
 use config::Config;
-use handlers::transfer;
+use handlers::{
+    transfer,
+    get_accounts,
+    credit_account,
+    get_transactions,
+};
 
 #[tokio::main]
 async fn main() {
@@ -19,6 +27,9 @@ async fn main() {
 
     let app = Router::new()
         .route("/transfer", post(transfer))
+        .route("/accounts", get(get_accounts))
+        .route("/accounts/:id/credit", post(credit_account))
+        .route("/transactions/:account_id", get(get_transactions))
         .layer(CorsLayer::permissive())
         .with_state(config);
 
